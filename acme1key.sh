3933be6c106c93252f02ada1b3611afa639644b5
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-04-27 01:30:11 +0800

    Update acme1key.sh

--- acme1key.sh
+++ acme1key.sh
@@ -101,7 +101,7 @@ getSingleCert(){
                 green "${domain} 解析结果：（$domainIP）"
                 red "当前域名解析的IP与当前VPS使用的真实IP不匹配"
                 green "建议如下："
-                yellow "1. 请确保CloudFlare为关闭状态(仅限DNS)，其他域名解析网站设置同理"
+                yellow "1. 请确保CloudFlare小云朵为关闭状态(仅限DNS)，其他域名解析网站设置同理"
                 yellow "2. 请检查DNS解析设置的IP是否为VPS的真实IP"
                 yellow "3. 脚本可能跟不上时代，建议截图发布到GitHub Issues或TG群询问"
                 exit 1
