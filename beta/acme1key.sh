a7ed76f4448b48f9d3cd66829e135d0f34646d6f
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-06-09 07:03:55 +0800

    Update acme1key.sh

--- beta/acme1key.sh
+++ beta/acme1key.sh
@@ -23,7 +23,7 @@ PACKAGE_UPDATE=("apt-get -y update" "apt-get -y update" "yum -y update" "yum -y 
 PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
 PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
-[[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
+[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
 
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
@@ -59,7 +59,7 @@ install_base(){
         ${PACKAGE_UPDATE[int]}
     fi
     ${PACKAGE_INSTALL[int]} curl wget sudo socat
-    if [[ $SYSTEM == CentOS ]]; then
+    if [[ $SYSTEM == $CentOS ]]; then
         ${PACKAGE_INSTALL[int]} cronie
         systemctl start crond
         systemctl enable crond
