45f5600cd89bcfe1e4e2b16d7da19feefe1be06e
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-06-26 22:19:05 +0800

    Update acme1key.sh

--- acme1key.sh
+++ acme1key.sh
@@ -73,11 +73,11 @@ install_base(){
 
 install_acme(){
     install_base
-    read -rp "请输入注册邮箱 (例: admin@gmail.com, 或留空自动生成一个gmail邮箱):" acmeEmail
+    read -rp "请输入注册邮箱 (例: admin@gmail.com, 或留空自动生成一个gmail邮箱): " acmeEmail
     if [[ -z $acmeEmail ]]; then
         autoEmail=$(date +%s%N | md5sum | cut -c 1-16)
         acmeEmail=$autoEmail@gmail.com
-        yellow "已取消设置邮箱, 使用自动生成的gmail邮箱：$acmeEmail"
+        yellow "已取消设置邮箱, 使用自动生成的gmail邮箱: $acmeEmail"
     fi
     curl https://get.acme.sh | sh -s email=$acmeEmail
     source ~/.bashrc
@@ -103,16 +103,16 @@ check_80(){
         ${PACKAGE_INSTALL[int]} lsof
     fi
     
-    yellow "检查 80 端口是否占用..."
+    yellow "正在检测80端口是否占用..."
     sleep 1
     
     if [[  $(lsof -i:"80" | grep -i -c "listen") -eq 0 ]]; then
-        green "目前 80 端口未被占用"
+        green "检测到目前80端口未被占用"
         sleep 1
     else
-        red "检测到 80 端口被其他程序被占用，以下为 80 端口的占用信息"
+        red "检测到目前80端口被其他程序被占用，以下为占用程序信息"
         lsof -i:"80"
-        read -rp "如需结束占用进程请按Y，按其他键则退出 [Y/N]：" yn
+        read -rp "如需结束占用进程请按Y，按其他键则退出 [Y/N]: " yn
         if [[ $yn =~ "Y"|"y" ]]; then
             lsof -i:"80" | awk '{print $2}' | grep -v "PID" | xargs kill -9
             sleep 1
@@ -123,7 +123,7 @@ check_80(){
 }
 
 acme_standalone(){
-    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装acme.sh，无法执行操作" && exit 1
+    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装acme.sh, 无法执行操作" && exit 1
     check_80
     WARPv4Status=$(curl -s4m10 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
     WARPv6Status=$(curl -s6m10 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
@@ -169,19 +169,19 @@ acme_standalone(){
 }
 
 acme_cfapiTLD(){
-    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装Acme.sh，无法执行操作" && exit 1
+    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装Acme.sh, 无法执行操作" && exit 1
     ipv4=$(curl -s4m10 https://ip.gs)
     ipv6=$(curl -s6m10 https://ip.gs)
-    read -rp "请输入需要申请证书的域名：" domain
+    read -rp "请输入需要申请证书的域名: " domain
     if [[ $(echo ${domain:0-2}) =~ cf|ga|gq|ml|tk ]]; then
-        red "检测为Freenom免费域名，由于CloudFlare API不支持，故无法使用本模式申请！"
+        red "检测为Freenom免费域名, 由于CloudFlare API不支持, 故无法使用本模式申请!"
         back2menu
     fi
-    read -rp "请输入CloudFlare Global API Key：" GAK
-    [[ -z $GAK ]] && red "未输入CloudFlare Global API Key，无法执行操作！" && exit 1
+    read -rp "请输入CloudFlare Global API Key: " GAK
+    [[ -z $GAK ]] && red "未输入CloudFlare Global API Key, 无法执行操作!" && exit 1
     export CF_Key="$GAK"
-    read -rp "请输入CloudFlare的登录邮箱：" CFemail
-    [[ -z $domain ]] && red "未输入CloudFlare的登录邮箱，无法执行操作！" && exit 1
+    read -rp "请输入CloudFlare的登录邮箱: " CFemail
+    [[ -z $domain ]] && red "未输入CloudFlare的登录邮箱, 无法执行操作!" && exit 1
     export CF_Email="$CFemail"
     if [[ -z $ipv4 ]]; then
         bash ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${domain}" -k ec-256 --listen-v6
@@ -193,20 +193,20 @@ acme_cfapiTLD(){
 }
 
 acme_cfapiNTLD(){
-    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装acme.sh，无法执行操作" && exit 1
+    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && red "未安装acme.sh, 无法执行操作" && exit 1
     ipv4=$(curl -s4m10 https://ip.gs)
     ipv6=$(curl -s6m10 https://ip.gs)
-    read -rp "请输入需要申请证书的泛域名（输入格式：example.com）：" domain
+    read -rp "请输入需要申请证书的泛域名 (输入格式：example.com): " domain
     [[ -z $domain ]] && red "未输入域名，无法执行操作！" && exit 1
     if [[ $(echo ${domain:0-2}) =~ cf|ga|gq|ml|tk ]]; then
-        red "检测为Freenom免费域名，由于CloudFlare API不支持，故无法使用本模式申请！"
+        red "检测为Freenom免费域名, 由于CloudFlare API不支持, 故无法使用本模式申请!"
         back2menu
     fi
-    read -rp "请输入CloudFlare Global API Key：" GAK
-    [[ -z $GAK ]] && red "未输入CloudFlare Global API Key，无法执行操作！" && exit 1
+    read -rp "请输入CloudFlare Global API Key: " GAK
+    [[ -z $GAK ]] && red "未输入CloudFlare Global API Key, 无法执行操作！" && exit 1
     export CF_Key="$GAK"
-    read -rp "请输入CloudFlare的登录邮箱：" CFemail
-    [[ -z $domain ]] && red "未输入CloudFlare的登录邮箱，无法执行操作！" && exit 1
+    read -rp "请输入CloudFlare的登录邮箱: " CFemail
+    [[ -z $domain ]] && red "未输入CloudFlare的登录邮箱, 无法执行操作!" && exit 1
     export CF_Email="$CFemail"
     if [[ -z $ipv4 ]]; then
         bash ~/.acme.sh/acme.sh --issue --dns dns_cf -d "*.${domain}" -d "${domain}" -k ec-256 --listen-v6
@@ -225,9 +225,9 @@ checktls() {
             fi
             sed -i '/--cron/d' /etc/crontab >/dev/null 2>&1
             echo "0 0 * * * root bash /root/.acme.sh/acme.sh --cron -f >/dev/null 2>&1" >> /etc/crontab
-            green "证书申请成功! 脚本申请到的证书 (cert.crt) 和私钥 (private.key) 已保存到 /root 文件夹下"
-            yellow "证书crt路径如下: /root/cert.crt"
-            yellow "私钥key路径如下: /root/private.key"
+            green "证书申请成功! 脚本申请到的证书 (cert.crt) 和私钥 (private.key) 文件已保存到 /root 文件夹下"
+            yellow "证书crt文件路径如下: /root/cert.crt"
+            yellow "私钥key文件路径如下: /root/private.key"
             back2menu
         else
             if [[ -n $(type -P wg-quick) && -n $(type -P wgcf) ]]; then
@@ -252,7 +252,7 @@ view_cert(){
 revoke_cert() {
     [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && yellow "未安装acme.sh, 无法执行操作!" && exit 1
     bash ~/.acme.sh/acme.sh --list
-    read -rp "请输入要撤销的域名证书 (复制Main_Domain下显示的域名):" domain
+    read -rp "请输入要撤销的域名证书 (复制Main_Domain下显示的域名): " domain
     [[ -z $domain ]] && red "未输入域名，无法执行操作!" && exit 1
     if [[ -n $(bash ~/.acme.sh/acme.sh --list | grep $domain) ]]; then
         bash ~/.acme.sh/acme.sh --revoke -d ${domain} --ecc
@@ -270,7 +270,7 @@ revoke_cert() {
 renew_cert() {
     [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && yellow "未安装acme.sh, 无法执行操作!" && exit 1
     bash ~/.acme.sh/acme.sh --list
-    read -rp "请输入要续期的域名证书 (复制Main_Domain下显示的域名):" domain
+    read -rp "请输入要续期的域名证书 (复制Main_Domain下显示的域名): " domain
     [[ -z $domain ]] && red "未输入域名, 无法执行操作!" && exit 1
     if [[ -n $(bash ~/.acme.sh/acme.sh --list | grep $domain) ]]; then
         bash ~/.acme.sh/acme.sh --renew -d ${domain} --force --ecc
